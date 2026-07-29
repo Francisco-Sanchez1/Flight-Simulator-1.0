f94e43387135de9ba1be8c75937e8a233723d2e9
Frank Sanchez <franksanchez132@gmail.com>
2026-07-29 06:46:27 -0500

    added getters and setters for speed, heading, and altitude

--- main.cpp
+++ main.cpp
@@ -1,12 +1,71 @@
 #include <iostream>
 
+char start_plane(){
+    char plane_start;
+    std::cout << "Welcome to flight simulator 1.0" << '\n';
+    std::cout << "Start up plane? (Y/N)" << '\n';
+    std::cin >> plane_start;
+
+    return plane_start;
+
+}
+
 class Plane{
     public:
+        void get_speed();
+        double set_speed(double s);
+        
+        void get_heading();
+        double set_heading(double h);
+
+        void get_altitude();
+        double set_altitude(double a);
+
+        Plane(double s, double h, double a) :speed{s}, heading{h}, altitude{a}{}
+
+
+    private: // Plane state
         double speed;
         double heading;
         double altitude;
-        Plane(double s, double h, double a) :speed{s}, heading{h}, altitude{a}{}
 };
+
+//Altitude Methods
+
+void Plane::get_altitude(){
+    std::cout << altitude << '\n';
+
+}
+double Plane::set_altitude(double a){
+    altitude = a;
+    return altitude;
+
+}
+
+
+//Heading Methods
+void Plane::get_heading(){
+    std::cout << heading << '\n';
+
+}
+double Plane::set_heading(double h){
+    heading = h;
+    return h;
+
+}
+
+//Speed Methods
+
+void Plane::get_speed(){
+    std::cout << speed << '\n';
+
+}
+
+double Plane::set_speed(double s){
+        speed = s;
+        return speed;
+}
+
 
 int main(){
     char plane_start;
@@ -16,9 +75,11 @@ int main(){
 
 
     //Welcome message
-    std::cout << "Welcome to flight simulator 1.0" << '\n';
-    std::cout << "Start up plane? (Y/N)" << '\n';
-    std::cin >> plane_start;
+    plane_start = start_plane();
+    
+    if (plane_start == 'N'){
+       return 0;
+    }
 
     std::cout << "Please enter Speed: " << '\n';
     std::cin >> speed;
@@ -28,23 +89,22 @@ int main(){
 
     std::cout << "Please enter altitude: " << '\n';
     std::cin >> altitude;
-
-
-    if (plane_start == 'N'){
-       return 0;
-
-    }
-    Plane p1 {speed, heading, altitude};
-
-    std::cout << "Speed: " << p1.speed << '\n';
-    std::cout << "Heading: " <<p1.heading << '\n';
-    std::cout << "Altitude: " << p1.altitude << '\n';
-
-    //initalize a plane
     
+    
+    Plane p1(speed, heading, altitude);
 
+    std::cout << '\n' << '\n';
 
+    p1.get_speed();
+    p1.set_speed(4343);
+    p1.get_speed();
 
-    std::cout << "Speed: " << p1.speed << '\n' << "Heading: " 
-    << p1.heading << '\n' <<"Altitude: " << p1.altitude;
+    p1.get_altitude();
+    p1.set_altitude(999);
+    p1.get_altitude();
+
+    p1.get_heading();
+    p1.set_heading(34.54545);
+    p1.get_heading();
+    
 }
